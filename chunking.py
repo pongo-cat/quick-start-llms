5277745c555b80a825adbe3f6a76428b641be72b
Alex Iannuzzi <emilyiannuzzi8@gmail.com>
2024-06-26 17:04:33 -0500

    fixed sixe chunking

--- chunking.py
+++ chunking.py
@@ -1,7 +1,7 @@
 #Code from 2.3 - 2.6 of various chunking methods
 
 import PyPDF2
-import re
+import tiktoken
 import numpy
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.pairwise import cosine_similarity
@@ -30,9 +30,39 @@ def no_chunking(file):
 
     return text
 
-#2.4, Fixed size chunking, non-overlapping
+#2.4, Fixed size chunking
 
-#2.4, Fixed size chunking, overlapping
+def fixed_size_chunking(file, chunk_size, overlap = 0, text = None):
+    """
+    Function to extract text from a PDF file with fixed size chunking
+    __________
+    Parameters:
+    file: str
+        Path to the PDF file
+    chunk_size: int
+        Number of characters in each chunk
+    overlap: int
+        Number of characters to overlap between chunks
+    text: str
+        Text extracted from the PDF file (if already extracted)
+    __________
+    Returns:
+    chunks: str
+        Chunks extracted from the PDF file
+
+    """
+    if text is None:
+        with open(file, 'rb') as f:
+            reader = PyPDF2.PdfReader(f)
+            text = ""
+            for page in range(reader.pages):
+                new_text = page.extract_text()
+                text += '\n\n' + new_text #Edit to add starting point
+            text = text.strip()
+
+    chunks = []
+    sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+    n_tokens = [len(sentence.split()) for sentence in sentences]
 
 #2.5, Chunking based on a delimiter/ natural whitespace
 
